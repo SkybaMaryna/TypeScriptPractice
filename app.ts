97a1119c578dd92d67fd1f3a22637477162a08b1
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-06-01 14:30:37 +0300

    Lesson1-2

---
+++ app.ts
@@ -0,0 +1,97 @@
+
+const button = document.querySelector("button")! as HTMLButtonElement;
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function() {
+  console.log(add(+input1.value, +input2.value));
+});
+
+let age: number;
+age = 50;
+let name1: string;
+name1 = 'Max';
+let toggle: boolean;
+toggle = true;
+let empty: null;
+empty = null;
+let notInitialize: undefined;
+notInitialize = undefined;
+let callback: (a: number)  => number;
+callback = (a) => { return 100 + a };
+
+let anything: any;
+anything = -20;
+anything = 'Text';
+anything = {};
+
+let some:unknown;
+some = 'Text';
+let str: string;
+if(typeof some === "string") {
+    str = some
+}
+
+let person : [string, number];
+person = ['Max', 21];
+
+
+enum Load {LOADING, READY};
+const page = {
+    load: Load.READY
+}
+if (page.load === Load.LOADING) {
+    console.log('Сторінка завантажується');
+}else {
+    console.log('Сторінка завантажена')}
+
+
+let union: string | number;
+
+let literal: 'enable' | 'disable';
+
+function showMessage(message: string) :void {
+    console.log(message);
+}
+  
+function calc(num1: number, num2: number) :number {
+    return num1 + num2;
+}
+  
+function customError() :never {
+    throw new Error('Error');
+}
+
+const page1: DataType = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+      createAt: new Date('2021-01-01'),
+      updateAt: new Date('2021-05-01'),
+    }
+  }
+  
+  const page2: DataType = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+  }
+
+  type DataType = {
+    title: string,
+    likes: number,
+    accounts: string[],
+    status: 'open' | 'close',
+    details?: {
+        createAt: Date,
+        updateAt: Date,
+    }
+  }
+  
